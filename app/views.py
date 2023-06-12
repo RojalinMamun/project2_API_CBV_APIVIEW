6115790ec96eaee7614afb30063c3ca6cbe632a0
Rojalin Das <dasrosalin670@gmail.com>
2023-06-12 21:45:24 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -9,12 +9,12 @@ from rest_framework.response import Response
 from rest_framework import status
 
 class ProductCrud(APIView):
-    def get(self,request):
+    def get(self,request,id):
         PQS=Product.objects.all()
         PJD=ProductSerializer(PQS,many=True)
         return Response(PJD.data)
 
-    def post(self,request):
+    def post(self,request,id):
         PMSD=ProductSerializer(data=request.data)
         if PMSD.is_valid():
             PMSD.save()
@@ -22,7 +22,7 @@ class ProductCrud(APIView):
 
         return Response({'failed':'Product creation is failed' })
 
-    def put(self,request):
+    def put(self,request,id):
         id=request.data['id']
         PO=Product.objects.get(id=id)
         UPO=ProductSerializer(PO,data=request.data)
@@ -31,5 +31,16 @@ class ProductCrud(APIView):
             return Response({'message': 'Product is Updated'})
 
         return Response({'failed':'Product is not Updated' })
+
+    def patch(self,request,id):
+        id=request.data['id']
+        PO=Product.objects.get(id=id)
+        PO.Pname=request.data['Pname']
+        PO.save()
+        return Response({'Success':'Data is Partially Updated'})
+
+    def delete(self,request,id):
+        Product.objects.get(id=id).delete()
+        return Response({'Success':'Product is deleted'})
 
 
